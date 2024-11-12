58a8ca5cf4186673d1fabfbd125c914f5aa6e7f9
IceLater <120577848+icelaterdc@users.noreply.github.com>
2024-11-12 03:30:23 +0300

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -1,11 +1,11 @@
 export const config = {
   discord: {
-    id: "YOUR_DISCORD_ID",
-    username: "parsher",
-    avatar: "https://cdn.discordapp.com/avatars/YOUR_DISCORD_ID/YOUR_AVATAR_HASH.png"
+    id: "991409937022468169",
+    username: "marjinalmuhammed",
+    avatar: "https://media.discordapp.net/attachments/1076802612386615386/1305690692479352842/e028d4e9e2dd29c748404a13f7eee1f6.png?ex=6733f2b7&is=6732a137&hm=540ddf4e0c0225b109aca1467035a6457f8ed0b2c81b0f33f81e034aa24d19b8&"
   },
   personal: {
-    name: "Parsher",
+    name: "IceLater",
     title: "Full-Stack Developer • Content Creator",
     pronouns: "he/him",
     age: 17,
@@ -14,10 +14,10 @@ export const config = {
     hobbies: ["digital drawing", "making youtube videos", "software"]
   },
   social: {
-    github: "parsherr",
-    discord: "parsher",
-    youtube: "parsher_",
-    deviantart: "itzParsher"
+    github: "icelaterdc",
+    discord: "icelater",
+    youtube: "parsher",
+    deviantart: "itzIceLater"
   },
   stack: [
     { name: "JavaScript", icon: "js" },
